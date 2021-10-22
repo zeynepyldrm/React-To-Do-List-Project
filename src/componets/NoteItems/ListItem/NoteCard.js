bfbbe1653a1ac5562554d39dcf666f850aa66e38
mrdlypenyez <zeynepyldrm1516@gmail.com>
2021-10-22 18:16:28 +0300

    clicked note deleted working

--- src/componets/NoteItems/ListItem/NoteCard.js
+++ src/componets/NoteItems/ListItem/NoteCard.js
@@ -1,22 +1,61 @@
 import React, { Component } from 'react'
 
+import axios from "axios";
+import { Link } from 'react-router-dom';
 class NoteCard extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            clickedNoteId: ""
+        }
 
-    render(){
-    return (
+    }
+    DelNote = (id) => {
 
-        <div class="card" style={{marginBottom:"20px"}}>{this.props.key}
-            <div class="card-header">{this.props.date}
+        console.log("function çalıştı");
+        const baseURL = "http://localhost:3333/notes"
+        let note = null
+        axios.get(`${baseURL}/${id}`)
+            .then((response) => { 
+                note = response.data 
+                if (note != null) {
+                    axios.delete(`${baseURL}/${id}`)
+                        .then(() => {
+                            alert("Note deleted!");
+                            window.location.reload();
+                            note = null
+                        }
+                        )
+                }
+                else{
+                    console.log("null value")
+                }
+            })
+        
+        
+
+    }
+
+    render() {
+        return (
+
+            <div className="card" style={{ marginBottom: "20px" }}>{ this.props.key}
+                <div className="card-header">{this.props.date}
+                </div>
+                <div className="card-body">
+                    <h5 className="card-title">{this.props.title}</h5>
+                    <p className="card-text">{this.props.content}</p>
+                    <a href="#" className="btn btn-primary">Edit</a>
+                    <button className="btn btn-danger" type="button" onClick={() => {
+
+                        this.DelNote(this.props.id)
+                    }}><i className="fa fa-trash"></i></button>
+
+                </div>
             </div>
-            <div class="card-body">
-                <h5 class="card-title">{this.props.title}</h5>
-                <p class="card-text">{this.props.content}</p>
-                <a href="#" class="btn btn-primary">Clicked</a>
-            </div>
-        </div>
 
-    )
-}
+        )
+    }
 
 }
 export default NoteCard;
--- src/componets/NoteItems/ListItem/NoteList.js
+++ src/componets/NoteItems/ListItem/NoteList.js
@@ -1,7 +1,5 @@
 import React, { Component } from 'react'
 import NoteCard from './NoteCard'
-import { ListGroup, ListGroupItem } from 'reactstrap';
-import { Table } from 'reactstrap';
 class NoteList extends Component {
     constructor(props) {
         super(props);
@@ -23,7 +21,7 @@ class NoteList extends Component {
         return (
             <div className="container">{
                 this.state.notes.map(note => (
-                <NoteCard title={note.title} date={note.date} content={note.content} key={note.id} />
+                <NoteCard title={note.title} date={note.date} content={note.content} key={note.id} id={note.id} />
             ))
             }
             </div>
